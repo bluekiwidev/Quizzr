14c0324babad509538afd819d10f102361f70e3f
Beijing-corn87 <111331826+bluekiwidev@users.noreply.github.com>
2026-08-01 20:59:46 +1200

    Added some elements

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,15 +1,32 @@
-<script lang="ts">
-	import { TextMarquee } from "$lib/components/spell/text-marquee";
-
-	let phrases = ["fun", "engaging", "active", "competitive", "easy", "interactive", "rewarding", "social", "immersive", "personalized"];
+<script>
+	import { DiaTextReveal } from "$lib/components/magic/dia-text-reveal";
 </script>
 
-<TextMarquee items={phrases} height={120} itemHeight={40} speed={1}>
-	{#snippet prefix()}
-		<span class="text-muted-foreground/75 text-3xl font-medium">Make learning </span>
-	{/snippet}
+<div class="bg-emerald-700 flex-col items-center gap-4 py-4 px-5">
+	<h1 class="text-4xl font-bold tracking-tight text-white sm:text-4xl text-left">Voxel</h1>
+</div>
 
-	{#snippet children(item)}
-		<span class="text-3xl font-medium">{item}</span>
-	{/snippet}
-</TextMarquee>
+<div class="text-foreground flex min-h-64 items-center justify-center p-8">
+  <h1 class="text-[clamp(1rem,7vw,5.5rem)] text-center font-semibold tracking-tight leading-none text-white">
+    <span>Make learning</span>{" "}
+    <span class="inline-block">
+      <DiaTextReveal
+        repeat
+        repeatDelay={1.2}
+        text={[
+          "fun",
+          "engaging",
+          "active",
+          "competitive",
+          "easy",
+          "interactive",
+          "rewarding",
+          "social",
+          "immersive"
+        ]}
+      />
+    </span>
+  </h1>
+</div>
+
+<h2 class="text-3xl font-semibold tracking-tight text-white text-center">With Voxel</h2>
