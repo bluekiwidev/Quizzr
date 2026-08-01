0dd95d1ff9092bafea7651b6366432c678bfbde3
bluekiwidev <111331826+bluekiwidev@users.noreply.github.com>
2026-08-02 01:02:30 +1200

    Moved navbar to seperate file

---
+++ frontend/src/lib/components/navbar.svelte
@@ -0,0 +1,3 @@
+<div class="bg-emerald-700 flex-col items-center gap-4 py-4 px-5">
+	<h1 class="text-4xl font-bold tracking-tight text-foreground sm:text-4xl text-left">Voxel</h1>
+</div>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,20 +1,18 @@
 <script>
 	import { DiaTextReveal } from "$lib/components/magic/dia-text-reveal";
+	import Navbar from "$lib/components/navbar.svelte";
 </script>
 
-<div class="bg-emerald-700 flex-col items-center gap-4 py-4 px-5">
-	<h1 class="text-4xl font-bold tracking-tight text-white sm:text-4xl text-left">Voxel</h1>
-</div>
+<Navbar />
 
-<div class="text-foreground flex min-h-64 items-center justify-center p-8">
+<div class="flex min-h-64 items-center justify-center p-8">
   <h1 class="text-[clamp(1rem,7vw,5.5rem)] text-center font-semibold tracking-tight leading-none text-white">
-    <span>Make learning</span>{" "}
-    <span class="inline-block">
+    <span class="block text-foreground">Make learning</span>{" "}
+    <span class="block align-bottom leading-tight text-inherit font-extrabold pb-3">
       <DiaTextReveal
         repeat
         repeatDelay={1.2}
         text={[
-          "fun",
           "engaging",
           "active",
           "competitive",
@@ -22,7 +20,8 @@
           "interactive",
           "rewarding",
           "social",
-          "immersive"
+          "immersive",
+		  "fun again"
         ]}
       />
     </span>
