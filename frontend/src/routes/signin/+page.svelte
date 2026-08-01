8acd0abf2fd7305bbe38224b32b8bc3c0de216cc
bluekiwidev <111331826+bluekiwidev@users.noreply.github.com>
2026-08-02 01:02:35 +1200

    updated style

---
+++ frontend/src/routes/signin/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+	import { DiaTextReveal } from "$lib/components/magic/dia-text-reveal";
+</script>
+
+<div class="bg-emerald-700 flex-col items-center gap-4 py-4 px-5">
+	<h1 class="text-4xl font-bold tracking-tight text-foreground sm:text-4xl text-left">Voxel</h1>
+</div>
