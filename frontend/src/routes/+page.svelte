7489dab405e89a2daf5776315e52b34d92934b98
bluekiwidev <111331826+bluekiwidev@users.noreply.github.com>
2026-08-01 15:58:41 +1200

    added magic ui deps added first element

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { TextMarquee } from "$lib/components/spell/text-marquee";
+
+	let phrases = ["fun", "engaging", "active", "competitive", "easy", "interactive", "rewarding", "social", "immersive", "personalized"];
+</script>
+
+<TextMarquee items={phrases} height={120} itemHeight={40} speed={1}>
+	{#snippet prefix()}
+		<span class="text-muted-foreground/75 text-3xl font-medium">Make learning </span>
+	{/snippet}
+
+	{#snippet children(item)}
+		<span class="text-3xl font-medium">{item}</span>
+	{/snippet}
+</TextMarquee>
