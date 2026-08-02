d9abc99dda6e3937b1f230c5f7ddf208de8b0d19
coolguycoder <yt.sleep69@gmail.com>
2026-08-02 21:02:47 +1200

    NOT FULLY WORKING TABLES FUNCTION. Nearly implemented auto creation of tables if they dont exist.

--- backend/src/apihandler.go
+++ backend/src/apihandler.go
@@ -14,12 +14,12 @@ func startwebserver() {
 	// Grab .env stuff
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Couldnt find .env file in backend dir. HINT: Is the .env actually in the backend dir?")
+		log.Fatal("\n Couldnt find .env file in backend dir. HINT: Is the .env actually in the backend dir?")
 	}
 	PORT := os.Getenv("WebserverPort")
 
 	// Start webserver
-	fmt.Println("Starting Webserver on Port ", PORT)
+	fmt.Println("\n Starting Webserver on Port ", PORT)
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "index.html")
