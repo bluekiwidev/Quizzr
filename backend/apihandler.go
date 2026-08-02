148ce50c3b37b2ac013ce6da9babaae3085bcde1
coolguycoder <yt.sleep69@gmail.com>
2026-08-02 17:39:57 +1200

    Refactor webserver to load port from .env and update comments for clarity

--- backend/apihandler.go
+++ backend/apihandler.go
@@ -5,15 +5,27 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/joho/godotenv"
 )
 
 func startwebserver() {
-	fmt.Println("Starting Webserver on Port 8080")
+	// Grab .env stuff
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Couldnt find .env file in backend dir. HINT: Is the .env actually in the backend dir?")
+	}
+	PORT := os.Getenv("WebserverPort")
+
+	// Start webserver
+	fmt.Println("Starting Webserver on Port ", PORT)
 
 	responseHandler := func(w http.ResponseWriter, req *http.Request) {
 		io.WriteString(w, "Hello, world!\n")
 	}
 
 	http.HandleFunc("/main", responseHandler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(PORT, nil))
 }
