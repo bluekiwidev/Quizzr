307b4c5c5d2815a7a8b261c56f23c341d77fce59
bluekiwidev <111331826+bluekiwidev@users.noreply.github.com>
2026-08-03 21:22:19 +1200

    Added signup button

--- frontend/src/lib/components/navbar.svelte
+++ frontend/src/lib/components/navbar.svelte
@@ -1,4 +1,5 @@
 <div class="bg-emerald-700 flex-col items-center gap-4 py-4 px-5">
-	<h1 class="text-4xl font-bold tracking-tight text-foreground sm:text-4xl text-left">Voxel</h1>
+	<a href="/"><h1 class="text-4xl font-bold tracking-tight text-foreground sm:text-4xl text-left">Voxel</h1></a>
 	<a href="/signin" class="text-foreground text-sm font-semibold tracking-tight hover:underline bg-primary text-primary-foreground p-2">Sign in</a>
+	<a href="/signup" class="text-foreground text-sm font-semibold tracking-tight hover:underline bg-primary text-primary-foreground p-2">Sign up</a>
 </div>
