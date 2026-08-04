3096e634b5e4304311e2be7c976b43a889bbc2e4
Beijing-corn87 <111331826+bluekiwidev@users.noreply.github.com>
2026-08-04 18:20:36 +1200

    optimised sub-optimal code

--- backend/src/databasehandle.go
+++ backend/src/databasehandle.go
@@ -11,19 +11,19 @@ import (
 	"github.com/joho/godotenv"
 )
 
+var dbName = os.Getenv("DB_NAME")
+var dbUser = os.Getenv("DB_USER")
+var dbPword = os.Getenv("DB_PWORD")
+var dbIP = os.Getenv("DB_IP")
+var dbPort = os.Getenv("DB_PORT")
+
 func dbstartup() {
 	fmt.Println("Starting DB")
 
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("\n Couldnt find .env file in backend dir. HINT: Is the .env actually in the backend dir?")
+		fmt.Println("\n Couldnt find .env file in backend dir. HINT: Is the .env actually in the backend dir? WILL BE USING ENV VARS")
 	}
-
-	dbName := os.Getenv("DB_NAME")
-	dbUser := os.Getenv("DB_USER")
-	dbPword := os.Getenv("DB_PWORD")
-	dbIP := os.Getenv("DB_IP")
-	dbPort := os.Getenv("DB_PORT")
 
 	dsn := fmt.Sprintf("%s:%s@tcp(%s%s)/%s?parseTime=true", dbUser, dbPword, dbIP, dbPort, dbName)
 	fmt.Println(dsn, "\n")
@@ -95,14 +95,8 @@ func createtables(db *sql.DB, currentTables []string) {
 func usernamevalidcheck(username string) int {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("\n Couldnt find .env file in backend dir. HINT: Is the .env actually in the backend dir?")
+		fmt.Println("\n Couldnt find .env file in backend dir. HINT: Is the .env actually in the backend dir? WILL BE USING ENV VARS")
 	}
-
-	dbName := os.Getenv("DB_NAME")
-	dbUser := os.Getenv("DB_USER")
-	dbPword := os.Getenv("DB_PWORD")
-	dbIP := os.Getenv("DB_IP")
-	dbPort := os.Getenv("DB_PORT")
 
 	dsn := fmt.Sprintf("%s:%s@tcp(%s%s)/%s?parseTime=true", dbUser, dbPword, dbIP, dbPort, dbName)
 	fmt.Println(dsn, "\n")
