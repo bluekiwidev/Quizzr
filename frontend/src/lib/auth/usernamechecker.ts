873fe6badc7bdaee1d56941bdc89a9bd548c0655
Beijing-corn87 <111331826+bluekiwidev@users.noreply.github.com>
2026-08-04 18:25:49 +1200

    fix: remove unnecessary username trimming in signup form

--- frontend/src/lib/auth/usernamechecker.ts
+++ frontend/src/lib/auth/usernamechecker.ts
@@ -6,11 +6,10 @@ export default async function checkUsernameAvailability(username: string): Promi
       `${PUBLIC_BACKEND}/checkusername?username=${encodeURIComponent(username)}`,
       {
         method: "GET",
-        headers: { "Content-Type": "application/json" }
       }
     );
     console.log("code", response.status)
-      
+
     //0 is ok, 1 is taken, 2 is error
     if (response.status === 409) return 1; // Username choice has a conflict (already taken)
     if (response.status !== 200) return 2; // Error in the request, treat as unavailable
