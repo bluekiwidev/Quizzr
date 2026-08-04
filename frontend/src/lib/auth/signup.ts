70b4dd8d780232f992f83b536af5e7bd71ad7b62
Beijing-corn87 <111331826+bluekiwidev@users.noreply.github.com>
2026-08-04 21:16:47 +1200

    Added request logic

--- frontend/src/lib/auth/signup.ts
+++ frontend/src/lib/auth/signup.ts
@@ -1,3 +1,23 @@
-export default async function sendSignupRequest(email: string, username: string, password: string) {
-    //TBD
+import { PUBLIC_BACKEND } from "$env/static/public";
+
+export default async function sendSignupRequest(email: string, username: string, password: string): Promise<number> {
+    try {
+        const response = await fetch(
+          `${PUBLIC_BACKEND}/submitsignup`,
+          {
+            method: "POST",
+            body: JSON.stringify(
+                { Email: email, Username: username, Password: password }),
+          }
+        );
+
+        // Return codes:
+        if (response.status === 201) return 0; //All good
+        if (response.status === 409) return 1; //Username or email already exists
+        if (response.status === 400) return 2; //Invalid input
+        if (response.status === 500) return 3; //Server error
+        return 4;
+      } catch {
+        return 4;
+      }
 }
